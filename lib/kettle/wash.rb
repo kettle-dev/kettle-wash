9819b1c14789cdd16131ddf8f131a436f702fd4e
Peter H. Boling <peter.boling@gmail.com>
2026-03-13 21:49:59 -0600

    🎉 init

---
+++ lib/kettle/wash.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "wash/version"
+
+module Kettle
+  module Wash
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/kettle/wash/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Kettle
+  module Wash
+    VERSION = "0.1.0"
+  end
+end
